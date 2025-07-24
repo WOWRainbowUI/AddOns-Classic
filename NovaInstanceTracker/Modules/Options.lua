83023a556892684374bf1a4f35f47f4241cc6566
Dololo <class2u@users.noreply.github.com>
2025-07-24 17:07:06 +0800

    更新 Nova 副本進度追蹤 1.99
    # Conflicts:
    #	NovaInstanceTracker/Locale/esES.lua
    #	NovaInstanceTracker/Locale/esMX.lua
    #	NovaInstanceTracker/Locale/frFR.lua
    #	NovaInstanceTracker/Locale/ptBR.lua
    #	NovaInstanceTracker/Modules/Options.lua

--- NovaInstanceTracker/Modules/Options.lua
+++ NovaInstanceTracker/Modules/Options.lua
@@ -16,7 +16,7 @@ NIT.options = {
 	args = {
 		titleText = {
 			type = "description",
-			name = "        " .. NIT.prefixColor .. L["Nova Instance Tracker"] .." (v" .. NIT.version .. ")",
+			name = "        " .. NIT.prefixColor .. L["Nova Instance Tracker"] .." (v" .. string.format("%.2f", NIT.version) .. ")",
 			fontSize = "large",
 			order = 1,
 		},
